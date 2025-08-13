aa3c32a96f798cdfb4f5e21c4ce4b902a043c4e7
david <dmsim159@gmail.com>
2025-08-12 18:16:07 -0700

    Store created and updated timestamps

--- backend/bistro/api/models.py
+++ backend/bistro/api/models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Timestamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
