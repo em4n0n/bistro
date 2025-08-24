195ae27db233a597dba2eef73f3008dc1859f837
david <dmsim159@gmail.com>
2025-08-24 12:42:26 -0700

    Creaate Category Serializer

--- backend/bistro/api/serializers.py
+++ backend/bistro/api/serializers.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from .models import Category, MenuItem, Order, OrderItem
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "slug"]
