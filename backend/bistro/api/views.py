2d53154bcd8594bdb143768fb21606e932dab13a
david <dmsim159@gmail.com>
2025-08-26 21:10:33 -0700

    Import methods

--- backend/bistro/api/views.py
+++ backend/bistro/api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
