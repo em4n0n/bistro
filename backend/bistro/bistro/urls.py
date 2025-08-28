50d4bd54dcbcab21d981ffe9af114608cbb71a42
david <dmsim159@gmail.com>
2025-08-27 21:13:10 -0700

    Import modules

--- backend/bistro/bistro/urls.py
+++ backend/bistro/bistro/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
