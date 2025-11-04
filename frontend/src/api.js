7e4d4c1b1f1789139643ee46bd8ad8a7515ce6c3
david <dmsim159@gmail.com>
2025-11-03 23:03:05 -0800

    Add JWT to API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,3 +4,9 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api',
   timeout: 10000,
 });
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
